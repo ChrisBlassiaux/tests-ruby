c6c7d6236b934bbf7c92e198dc95f7ad059779e1
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-04-13 21:41:35 +0200

    Correct 04

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -1,3 +1,49 @@
-def echo()
-
+def echo(word)
+  return word
 end
+
+#p echo("hello")
+
+def shout(word)
+  return word.upcase
+end
+
+#p shout("hello world")
+
+def repeat(word, multiply = 2)
+  array = []
+  multiply.times do |i|
+    array << word
+  end
+  return array.join(" ")
+end
+
+#p repeat("hello", 10)
+
+def start_of_word(str, nb = 1)
+  array = str.split(//)
+  array_new = []
+  counter = 0
+  array.each do |i|
+    if counter != nb
+      array_new << i
+      counter += 1
+    end
+  end
+  return array_new.join
+end
+
+#p start_of_word("abcdefg", 3)
+
+def start_of_word(str)
+  array = str.split(/ /)
+  array.first
+end
+
+#p start_of_word("Hello soi")
+
+def titleize(str)
+  str.split.map(&:capitalize).join(' ')
+end
+
+#p titleize("ceci est un string")
